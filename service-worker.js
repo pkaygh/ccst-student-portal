9e653a2029856993dffd0195743f5ddcdc2d4f7d
pkaygh <53969211+pkaygh@users.noreply.github.com>
2026-03-26 00:59:28 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,21 @@
-const CACHE_NAME = 'ccst-portal-v1';
+const CACHE_NAME = "ccst-cache-v1";
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
